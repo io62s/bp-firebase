3854fc1988d30bd2c4e4539d96ab7868e421d934
io62s <ufo62s@gmail.com>
2019-11-22 19:55:43 +0100

    posts, auth state moved to context

--- src/components/App.js
+++ src/components/App.js
@@ -1,53 +1,16 @@
-import React, { Component } from "react";
+import React from "react";
 import Authentication from "./Authentication";
-import { firestore, auth, createUserProfileDocument } from "../firebase";
-import { collectIdsAndDocs } from "../utilities";
 
 import Posts from "./Posts";
 
-class Application extends Component {
-  state = {
-    posts: [],
-    user: null
-  };
+const App = () => {
+  return (
+    <main className="Application">
+      <h1>Blog</h1>
+      <Authentication />
+      <Posts />
+    </main>
+  );
+};
 
-  unsubscribeFromFirestore = null;
-  unsubscribeFromAuth = null;
-
-  getPosts = () => {
-    this.unsubscribeFromFirestore = firestore
-      .collection("posts")
-      .onSnapshot(snapshot => {
-        const posts = snapshot.docs.map(collectIdsAndDocs);
-        this.setState({ posts });
-      });
-  };
-
-  componentDidMount = () => {
-    this.getPosts();
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
-      const user = await createUserProfileDocument(userAuth);
-      console.log(user);
-
-      this.setState({ user });
-    });
-  };
-
-  componentWillUnmount = () => {
-    this.unsubscribeFromFirestore();
-  };
-
-  render() {
-    const { posts, user } = this.state;
-
-    return (
-      <main className="Application">
-        <h1>Blog</h1>
-        <Authentication user={user} />
-        <Posts posts={posts} />
-      </main>
-    );
-  }
-}
-
-export default Application;
+export default App;
--- src/index.js
+++ src/index.js
@@ -2,5 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.scss";
 import App from "./components/App";
+import PostsProvider from "./providers/PostsProvider";
+import UserProvider from "./providers/UserProvider";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <UserProvider>
+    <PostsProvider>
+      <App />
+    </PostsProvider>
+  </UserProvider>,
+  document.getElementById("root")
+);
